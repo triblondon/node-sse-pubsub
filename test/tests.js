afe836d0de8c1b658e8a847812a1fc2ce6044517
Andrew Betts <andrew@trib.tv>
2021-10-25 16:44:07 +0100

    Update dependencies

--- test/tests.js
+++ test/tests.js
@@ -1,9 +1,10 @@
 
-const expect = require('chai').expect;
+import { expect}  from 'chai';
 
-const fetch = require('node-fetch');
-const http = require('http');
-const SSEChannel = require('../index');
+import fetch from 'node-fetch';
+import http from 'http';
+import SSEChannel from '../index.js';
+
 const PORT = process.env.PORT || 3102;
 const URL = "http://localhost:"+PORT+"/stream";
 
@@ -47,46 +48,53 @@ const measureTime = async pr => {
 describe('SSEChannel', function () {
 
 	it('should return 200 OK status', async function () {
-		await setupServer();
+		const sse = await setupServer();
     let res = await fetch(URL);
     expect(res.status).to.equal(200);
+		sse.close();
 	});
 
 	it('should have correct content-type', async function () {
-		await setupServer();
+		const sse = await setupServer();
 		let res = await fetch(URL);
     expect(res.headers.get('content-type')).to.equal('text/event-stream');
+		sse.close();
 	});
 
 	it('should be server-cachable for the duration of the stream', async function () {
-		await setupServer({maxStreamDuration:20000});
+		const sse = await setupServer({maxStreamDuration:20000});
 		let res = await fetch(URL);
     expect(res.headers.get('cache-control')).to.contain('s-maxage=19');
+		sse.close();
 	});
 
 	it('should not be client-cachable', async function () {
-		await setupServer({maxStreamDuration:20000});
+		const sse = await setupServer({maxStreamDuration:20000});
 		let res = await fetch(URL);
     expect(res.headers.get('cache-control')).to.contain('max-age=0');
+		sse.close();
 	});
 
 	it('should include retry in first response chunk', async function () {
-		let res, chunk;
-		await setupServer();
+		let sse, res, chunk;
+		sse = await setupServer();
 		res = await fetch(URL);
 		chunk = await nextChunk(res.body);
 		expect(chunk).to.match(/retry:\s*\d{4,6}\n/);
+		sse.close();
 
-		await setupServer({clientRetryInterval:1234});
+		sse = await setupServer({clientRetryInterval:1234});
 		res = await fetch(URL);
 		chunk = await nextChunk(res.body);
 		expect(chunk).to.match(/retry:\s*1234\n/);
+		sse.close();
 	});
 
 	it('should close the connection at maxStreamDuration', async function () {
-		await setupServer({maxStreamDuration:1500});
+		const sse = await setupServer({maxStreamDuration:1500});
 		let elapsed = await measureTime(fetch(URL).then(res => res.text()));
 		expect(elapsed).to.be.approximately(1500, 30);
+		sse.close();
 	});
 
 	it('should return an ID from publish', async function () {
@@ -101,6 +109,7 @@ describe('SSEChannel', function () {
 		output = await nextChunk(res.body);
 		expect((new RegExp(`id:\\s*${secondID}\\b`)).test(output)).to.be.true;
 		expect(secondID).to.equal(firstID+1);
+		sse.close();
 	});
 
 	it('should start at startId', async function () {
@@ -116,6 +125,8 @@ describe('SSEChannel', function () {
 		output = await nextChunk(res.body);
 		expect(output).to.match(/id:\s*2\n/);
 		expect(output).to.match(/data:\s*something else\n/);
+		sse.close();
+
 		sse = await setupServer({startId: 8777});
 		res = await fetch(URL);
 		await nextChunk(res.body);
@@ -123,10 +134,11 @@ describe('SSEChannel', function () {
 		output = await nextChunk(res.body);
 		expect(output).to.match(/id:\s*8777\n/);
 		expect(output).to.match(/data:\s*something\n/);
+		sse.close();
 	});
 
 	it('should ping at pingInterval', async function () {
-		await setupServer({pingInterval:500});
+		const sse = await setupServer({pingInterval:500});
 		let res = await fetch(URL);
 		await nextChunk(res.body)
 		let chunkPromise = nextChunk(res.body);
@@ -136,13 +148,15 @@ describe('SSEChannel', function () {
 		expect(output).to.match(/data:\s*\n\n/);
 		expect(output).to.not.contain('event:');
 		expect(output).to.not.contain('id:');
+		sse.close();
 	});
 
 	it('should output a retry number', async function () {
-		await setupServer({clientRetryInterval:4321});
+		const sse = await setupServer({clientRetryInterval:4321});
 		let res = await fetch(URL);
 		let chunk = await nextChunk(res.body);
 		expect(chunk).to.match(/retry:\s*4321\n\n/);
+		sse.close();
 	});
 
 	it('should include event name if specified', async function () {
@@ -156,6 +170,7 @@ describe('SSEChannel', function () {
 		sse.publish('with-event-name', 'myEvent');
 		chunk = await nextChunk(res.body);
 		expect(chunk).to.match(/event:\s*myEvent\n/);
+		sse.close();
 	});
 
 	it('should include only events the subscriber has specified', async function () {
@@ -171,6 +186,7 @@ describe('SSEChannel', function () {
 		sse.publish('baz', 'event-3');
 		chunk = await nextChunk(res.body);
 		expect(chunk).to.not.contain('event-3');
+		sse.close();
 	});
 
 	it('should include previous events if last-event-id is specified', async function () {
@@ -183,6 +199,7 @@ describe('SSEChannel', function () {
 		let events = output.split('\n\n').filter(str => str.includes('id:'));
 		expect(events.length).to.equal(6);
 		expect(events[0]).to.match(/id:\s*5\n/);
+		sse.close();
 	});
 
 	it('should limit history length to historySize', async function () {
@@ -195,6 +212,7 @@ describe('SSEChannel', function () {
 		let events = output.split('\n\n').filter(str => str.includes('id:'));
 		expect(events.length).to.equal(5);
 		expect(events[0]).to.match(/id:\s*6\n/);
+		sse.close();
 	});
 
 	it('should serialise non scalar values as JSON', async function () {
@@ -205,6 +223,7 @@ describe('SSEChannel', function () {
 		sse.publish(testdata);
 		let chunk = await nextChunk(res.body);
 		expect(chunk).to.contain(JSON.stringify(testdata));
+		sse.close();
 	});
 
 	it('should list connected clients by IP address', async function () {
@@ -214,6 +233,7 @@ describe('SSEChannel', function () {
 		expect(sse.listClients()).to.eql({
 			'::ffff:127.0.0.1': 2
 		});
+		sse.close();
 	});
 
 	it('should count connected clients', async function () {
@@ -221,6 +241,7 @@ describe('SSEChannel', function () {
 		let res1 = await fetch(URL);
 		let res2 = await fetch(URL);
 		expect(sse.getSubscriberCount()).to.equal(2);
+		sse.close();
 	});
 
 	it('should understand the rewind option', async function () {
@@ -234,6 +255,7 @@ describe('SSEChannel', function () {
 		expect(output).to.not.include('message-1');
 		expect(output).to.include('message-2');
 		expect(output).to.include('message-3');
+		sse.close();
 
 		sse = await setupServer({rewind: 6});
 		sse.publish('message-1');
@@ -244,6 +266,18 @@ describe('SSEChannel', function () {
 		expect(output).to.include('message-1');
 		expect(output).to.include('message-2');
 		expect(output).to.include('message-3');
+		sse.close();
+	});
+
+	it('should stop pinging and hang up when the stream is closed', async function () {
+		let sse, res;
+		let hasEnded = false;
+		sse = await setupServer();
+		res = await fetch(URL);
+		sse.close();
+		let respEndPromise = res.text();
+		let elapsed = await measureTime(respEndPromise);
+		expect(elapsed).to.be.lessThan(100, 30);
 	});
 
 });
